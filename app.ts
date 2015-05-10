449c28ca1808ed5a7d5628da0fff3ce876c135bb
Amin Meyghani <meyghania@gmail.com>
2015-05-10 18:11:26 -0400

    add the most basic root component

--- app.ts
+++ app.ts
@@ -1,2 +1,25 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
 
+@Component({
+  selector: 'root'
+})
+
+@View({
+  template: `
+  <div>
+    <h1>
+      {{ title }}
+    </h1>
+
+  </div>`
+})
+
+class Root {
+  title: string;
+  constructor() {
+    this.title = "Introduction to Angular2";
+  }
+}
+
+bootstrap(Root);
