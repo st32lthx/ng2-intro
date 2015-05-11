850bb65ce42f273572bb47b42039528a27812080
Amin Meyghani <meyghania@gmail.com>
2015-05-11 01:25:25 -0400

    add msgStore service, for loop for msgs

--- components/messenger/messenger.ts
+++ components/messenger/messenger.ts
@@ -1,27 +1,25 @@
 /// <reference path="../../typings/angular2/angular2.d.ts" />
-import {Component, View} from 'angular2/angular2';
+import {Component, View, For} from 'angular2/angular2';
 import {Compose} from "components/compose/compose"
+import {MsgStore} from "components/messenger/MsgStore";
 
 
 @Component({
-  selector: 'messenger'
+  selector: 'messenger',
+  injectables: [ MsgStore ]
 })
 
 @View({
   templateUrl: "components/messenger/messenger.tpl.html",
-  directives: [ Compose ]
+  directives: [ Compose, For ]
 })
 
 export class Messenger {
   title: string;
   messages: any[];
-  constructor() {
+  constructor(msgStore: MsgStore) {
     this.title = "Messenger Manager";
-    this.messages = [
-      { title: "hello", msg: "this is the hello message" },
-      { title: "meeting", msg: "this is the meeting message" },
-      { title: "goodby", msg: "this is the goodby message" }
-    ];
+    this.messages = msgStore.getAll();
   }
 }
 
