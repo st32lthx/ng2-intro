72d7bcafae45450a3d6d9568c874ab858d11cdef
Amin Meyghani <meyghania@gmail.com>
2015-05-11 01:41:44 -0400

    add event handler for compose, public get method

--- components/compose/compose.ts
+++ components/compose/compose.ts
@@ -12,13 +12,28 @@ import {Component, View} from 'angular2/angular2';
 
 @View({
   templateUrl: "components/compose/compose.tpl.html"
-
 })
 
 export class Compose {
   title: string;
+  iMsg: string;
   constructor() {
     this.title = "Composer";
+    this.iMsg = "";
+  }
+  // has to provide public api so other components
+  // can consume.
+  getMsg(msg) {
+    console.log(msg);
+    this.iMsg = msg;
+    return msg;
+  }
+  doneTyping($event) {
+    if($event.which === 13) {
+      console.log($event.target.value);
+      this.iMsg = $event.target.value;
+      $event.target.value = null;
+    }
   }
 }
 
