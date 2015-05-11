384d29837dec8284ebd4be2f1f84c1df79fcdca7
Amin Meyghani <meyghania@gmail.com>
2015-05-11 10:57:33 -0400

    clean up comments, add border to see the component
    Signed-off-by: Amin Meyghani <meyghania@gmail.com>

--- components/messenger/messenger.ts
+++ components/messenger/messenger.ts
@@ -23,22 +23,8 @@ export class Messenger {
     this.messages = msgStore.getAll();
     this.store = msgStore;
   }
-  // TODO: this exists here. It uses the store
-  // to add msgs. Now, we need to be able to use
-  // this when somehow. It is important to decide
-  // on the responsibilites of components. Does
-  // messenger handle the actual message creating?
-  // or does the composer handle the data formatting, reading
-  // etc? and simply compose provides a method called getInput
-  // and then we can call that from here and get the value
-  // and then just push it to our store?
+
   addMsg(title: string, body: string) {
     this.store.add({title: title, body: body});
   }
-  // we have access to compose. All we have to do
-  // is to listen for the setMsg event. once that
-  // is fired, we grab the instance variable,
-  // and we should be good ?
 }
-
-
